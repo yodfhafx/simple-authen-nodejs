fb9d760245ca1898669a53d9a15c6e7600a9930c
yodfhafx <yodfha2014@gmail.com>
2019-01-05 01:17:43 +0700

    create register and login form

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', {title: 'Register'});
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', {title: 'Login'});
 });
 
 module.exports = router;
